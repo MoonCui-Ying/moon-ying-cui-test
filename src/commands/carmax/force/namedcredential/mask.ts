b02245ef94673891181b840d5f855e31ab954167
Moon Y Cui <9502155@carmax.com>
2019-04-17 16:27:59 -0400

    add new command

---
+++ src/commands/carmax/force/namedcredential/mask.ts
@@ -0,0 +1,41 @@
+import { flags,SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+
+//const messages = Messages.loadMessages('moon-ying-cui-test', 'mask');
+
+export default class Mask extends SfdxCommand {
+
+    protected static requiresUsername = true;
+
+    /*protected static flagsConfig = {
+        // flag with a value (-a, --all)
+        all: flags.string({char: 'a', description: messages.getMessage('allFlagDescription')})
+        
+      };
+      */
+     
+    public async run(): Promise<AnyJson> {
+        const conn = this.org.getConnection();
+        const query = 'SELECT Id,DeveloperName,Endpoint,MasterLabel,PrincipalType FROM NamedCredential';
+
+        interface NamedCredential {
+            Id: string;
+            DeveloperName: string;
+            Endpoint: string;
+            MasterLabel: string;
+            PrincipalType: string;
+        }
+
+        const result = await conn.query<NamedCredential>(query);
+
+        if (!result.records || result.records.length <= 0) {
+            this.ux.log('No NamedCredentail found!');
+        }else{
+            for(let record of result.records){
+                this.ux.log(record.DeveloperName + ', ' + record.MasterLabel);
+            }
+        }
+        return;
+    }
+}
